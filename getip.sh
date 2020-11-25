a2c77504b04114a6976a5d082b73322d9abca485
“ahtwq” <ahnutwq@163.com>
2020-11-25 19:11:20 +0800

    capture IP

--- getip.sh
+++ getip.sh
@@ -5,8 +5,8 @@ cd /home/asus/tang/addmore/getIp
 date > serverIP.txt
 echo "IP: \c" >> serverIP.txt
 
-include_ip=$(curl www.trackip.net/i)
-ip=$(grep -Po '(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|[1-9])(\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)){3}' ${include_ip} | head -1)
+curl www.trackip.net/i > include_ip.txt
+ip=$(grep -Po '(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|[1-9])(\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)){3}' include_ip.txt | head -1)
 
 echo $ip >> serverIP.txt
 
@@ -19,8 +19,8 @@ git push -u origin master
 #do
 #    date > serverIP.txt
 #    echo "IP: " >> serverIP.txt
-#    curl www.trackip.net/i > iptemp.txt
-#    ip_new=$(grep -Po '(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|[1-9])(\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)){3}' iptemp.txt | head -1)
+#    curl www.trackip.net/i > include_ip.txt
+#    ip_new=$(grep -Po '(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|[1-9])(\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)){3}' include_ip.txt | head -1)
 #
 #	if [ "$ip" != "$ip_new" ]; then
 #		ip=$ip_new
